76790a005dc5926dd63d03eef3e0a49b3b4625b5
RAJ KUMAR <raj.kumar17460@marwadieducation.edu.in>
2021-05-08 22:24:41 +0530

    Create Practical_3.java

---
+++ Gtu_Practical/Practical_3.java
@@ -0,0 +1,19 @@
+/**
+ * Problem Statement: 
+ * Write a program that reads a number in meters, 
+ * converts it to feets, and display the result.
+ * (Hint: 1 metre = 3.28084 feet)
+ */
+package Gtu_Practical;
+import java.util.Scanner;
+public class Practical_3 {
+    public static void main(String[] args) {
+		System.out.print("Enter the value in Meters: ");
+		Scanner sc = new Scanner(System.in);
+		double num = sc.nextDouble();
+		double feet=num*3.28084;
+		System.out.println(num + " Meters =" + feet + " Feets");
+		        
+        sc.close();
+	}
+}
