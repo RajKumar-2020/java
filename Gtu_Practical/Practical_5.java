3606b8261dc4a0e3bb7f76a7bffb9f6776989a86
RAJ KUMAR <raj.kumar17460@marwadieducation.edu.in>
2021-05-08 22:26:13 +0530

    Create Practical_5.java

---
+++ Gtu_Practical/Practical_5.java
@@ -0,0 +1,35 @@
+/**
+ * Problem Statement:
+ * Write a program that prompts the user to 
+ * enter three integers and display the integers in decreasing order.
+ */
+package Gtu_Practical;
+import java.util.Scanner;
+public class Practical_5 {
+    public static void main(String[] args) {
+		int i,j,arr[]= {0,0,0};
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the three numbers:");
+		for(i=0;i<3;i++)
+			arr[i]=input.nextInt();
+
+		input.close();
+			
+		for(i=0;i<3-1;i++)
+			for(j=0;j<3-i-1;j++)
+				if(arr[j]<arr[j+1])
+				{
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp; 
+				}
+		
+		System.out.println("Numbers in the Decreasing order: ");
+		for(i=0;i<3;i++)
+			System.out.println(arr[i]);	
+		
+		input.close();        
+	}
+    
+}
