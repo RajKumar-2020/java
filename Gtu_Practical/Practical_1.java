4ae536019861e31ffeb597d5495c5afc0489fdda
RAJ KUMAR <raj.kumar17460@marwadieducation.edu.in>
2021-05-08 22:24:29 +0530

    Create Practical_1.java

---
+++ Gtu_Practical/Practical_1.java
@@ -0,0 +1,18 @@
+/** 
+ * Problem statement:
+ * Write a Program that displays 
+ * Hello World 
+ * Welcome to Java, 
+ * Learning Java Programming is fun.
+ * 
+ */
+package Gtu_Practical;
+
+public class Practical_1 {
+    public static void main(String[] args) 
+    {		
+        System.out.println(" Hello World ");
+        System.out.println(" Welcome to Java, \n Learning Java Programming is fun. ");
+    }
+    
+}
