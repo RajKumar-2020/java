c736e4bdd4ba57602899773e5fcb90ab5ff5020d
RAJ KUMAR <raj.kumar17460@marwadieducation.edu.in>
2021-05-08 23:07:21 +0530

    Create Practical_7.java

---
+++ Gtu_Practical/Practical_7.java
@@ -0,0 +1,26 @@
+package Gtu_Practical;
+
+public class Practical_7 {
+    public static void main(String[] args) {
+		
+		String numberPlate = ""+(char)(Math.random()*26+65);
+		numberPlate += (char)(Math.random()*26+65);
+		numberPlate += (char)(Math.random()*26+65);
+		
+		String digits = (""+ Math.random()).substring(3, 7);
+		System.out.println("Generated number plate value is: " 
+							+ numberPlate + " " +digits);		
+
+		numberPlate += (int)(Math.random()*10);
+		numberPlate += (int)(Math.random()*10);
+		numberPlate += (int)(Math.random()*10);
+		numberPlate += (int)(Math.random()*10);
+		
+		double num = Math.random();
+		System.out.println(num);
+		String str = ""+num;
+		System.out.println(str);
+		
+	}	
+    
+}
