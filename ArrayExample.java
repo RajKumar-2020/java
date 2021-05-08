30a6073f457cc24a22fae86fda1430f2e7d5f51c
RAJ KUMAR <raj.kumar17460@marwadieducation.edu.in>
2021-05-08 21:20:05 +0530

    Create ArrayExample.java

---
+++ ArrayExample.java
@@ -0,0 +1,14 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        double myList[] = new double[10];
+        for(double i : myList)
+        {
+            System.out.println("For Each: " + i);
+        }
+        for(double i=0 ; i < myList.length ; i++)
+        {
+            System.out.println("Simple For: " + i);
+        }
+    }
+    
+}
