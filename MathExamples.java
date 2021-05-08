c1d37fe70956b63e269fa0b814aa87559202d383
RAJ KUMAR <raj.kumar17460@marwadieducation.edu.in>
2021-05-08 21:20:52 +0530

    Create MathExamples.java

---
+++ MathExamples.java
@@ -0,0 +1,18 @@
+public class MathExamples {
+    public static void main(String[] args) {
+        System.out.println(Math.ceil(2.1));
+        System.out.println(Math.floor(2.9));
+        System.out.println(Math.rint(3.4));
+        System.out.println(Math.round(2.4f));
+        System.out.println(Math.max(11,10.0));
+        System.out.println(Math.min(11,10.0));
+        System.out.println(11 + 10.0);
+        System.out.println(Math.round(11 + 10.0));
+        System.out.println(Math.pow(11,2));
+        System.out.println(Math.exp(2));
+        System.out.println(Math.log(2));
+        System.out.println(Math.sqrt(121));
+        
+    }
+    
+}
