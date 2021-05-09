b8b01d370ea370b8d0a2a1b8e17a13522dff6867
RAJ KUMAR <raj.kumar17460@marwadieducation.edu.in>
2021-05-09 12:31:07 +0530

    Create Practical_025.java

---
+++ Gtu_Practical/Practical_025.java
@@ -0,0 +1,32 @@
+/* 
+* Program - 25
+* Write a program that reads words from a text file and displays 
+* all the nonduplicate words in descending order. The text file is 
+* passed as a command-line argument.
+*/
+package Gtu_Practical;
+import java.io.*;
+import java.util.*;
+public class Practical_025 {
+    public static void main(String[] args) {
+        //   if(args.length==1){
+        //   String fileName = args[0];
+            String fileName = "C://list.txt";
+            TreeSet<String> set = new TreeSet<>();
+            File file = new File(fileName);
+            try {
+                Scanner s = new Scanner(file);
+                while (s.hasNext()){
+                    set.add(s.next());
+                }
+                System.out.println(set);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        // }
+        // else{
+        //     System.out.println("Please, Pass the File Name as Command Line Argument");
+        // }
+    }
+    
+}
